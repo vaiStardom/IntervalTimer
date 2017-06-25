4ae502891aaada3692ced34bb987bb5f2dc990ef
Paul Addy <paul.addy@me.com>
2017-06-25 15:37:05 +0800

    made all navigation background colours black added all weather icons and custom add button added colours class

---
+++ IntervalTimer/IntervalTimerColors.swift
@@ -0,0 +1,23 @@
+//
+//  IntervalTimerColors.swift
+//  IntervalTimer
+//
+//  Created by Paul Addy on 2017-06-25.
+//  Copyright © 2017 Paul Addy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct IntervalTimerColors{
+    static let Black = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+    static let BlueProgress = UIColor(red: 109.0/255.0, green: 158.0/255.0, blue: 225.0/255.0, alpha: 1.0)
+    static let GreenProgress = UIColor(red: 125.0/255.0, green: 225.0/255.0, blue: 103.0/255.0, alpha: 1.0)
+    static let Orange = UIColor(red: 1, green: 149.0/255.0, blue: 0, alpha: 1.0)
+    static let OrangeProgress = UIColor(red: 250.0/255.0, green: 170.0/255.0, blue: 75.0/255.0, alpha: 1.0)
+    static let PurpleProgress = UIColor(red: 213.0/255.0, green: 143.0/255.0, blue: 231.0/255.0, alpha: 1.0)
+    static let RedProgress = UIColor(red: 247.0/255.0, green: 95.0/255.0, blue: 91.0/255.0, alpha: 1.0)
+    static let YellowProgress = UIColor(red: 252.0/255.0, green: 213.0/255.0, blue: 83.0/255.0, alpha: 1.0)
+    static let White = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+}
+
--- IntervalTimer/TimersViewController.swift
+++ IntervalTimer/TimersViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TimersViewController: UIViewController {
 
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+//    @IBOutlet weak var weatherIconImageView: UIImageView!
+//    @IBOutlet weak var temperatureLabel: UILabel!
+
+}
+//MARK: - Life-Cycle
+extension TimersViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
