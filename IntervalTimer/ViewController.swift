d106ef409339bbad0829dedc3cab580a6547a961
Paul Addy <paul.addy@me.com>
2017-06-25 09:47:12 +0800

    Initial Commit

---
+++ IntervalTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntervalTimer
+//
+//  Created by Paul Addy on 2017-06-25.
+//  Copyright © 2017 Paul Addy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
